0228d01b9b32b5af4ef7ffc30450f9743b8eed95
amankkg <amankubanychbek@gmail.com>
2019-01-17 01:01:49 +0300

    fix perms

--- perms.hs
+++ perms.hs
@@ -1,10 +1,8 @@
 module Perms where
 
--- perms :: [a] -> [[a]]
-perms l = f l where
-  f [] = [[]]
-  f [x] = [[x]]
-  f [x, y] = [[x,y], [y,x]]
-  f [x, y, z] = [[x,y,z], [x,z,y], [y,x,z], [y,z,x], [z,x,y], [z,y,x]]
-  f (x:xs) = map (x:) (f xs) ++ (map (++ [x]) (f xs))
-  -- f (x:xs) = (map ([x] ++) (f xs)) ++ (map (++ [x]) (f xs))
+perms :: [a] -> [[a]]
+perms [] = [[]]
+perms (a:as) = concatMap (f a) (perms as) where
+  f :: a -> [a] -> [[a]]
+  f a [] = [[a]]
+  f a (b:bs) = (a:b:bs) : (map (b:) (f a bs))
